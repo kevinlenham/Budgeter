642b82dce29dbba146ad00d178cbcd81b747ab33
Kevin-Le Nham <kevin.nham@hotmail.com>
2026-09-01 22:35:35 +1000

    xcode files

---
+++ Budgeter/BudgeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeterApp.swift
+//  Budgeter
+//
+//  Created by Kevin-Le Nham on 1/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgeterTests/BudgeterTests.swift
@@ -0,0 +1,19 @@
+//
+//  BudgeterTests.swift
+//  BudgeterTests
+//
+//  Created by Kevin-Le Nham on 1/9/2026.
+//
+
+import Testing
+@testable import Budgeter
+
+struct BudgeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
