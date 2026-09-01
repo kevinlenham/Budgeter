f68c213230568ffa7ad39e2179a2b387ab3bec47
Kevin-Le Nham <kevin.nham@hotmail.com>
2026-09-01 22:56:48 +1000

    Added tooling

--- BudgeterUITests/BudgeterUITestsLaunchTests.swift
+++ BudgeterUITests/BudgeterUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class BudgeterUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
@@ -18,7 +17,7 @@ final class BudgeterUITestsLaunchTests: XCTestCase {
     }
 
     @MainActor
-    func testLaunch() throws {
+    func testLaunch() {
         let app = XCUIApplication()
         app.launch()
 
